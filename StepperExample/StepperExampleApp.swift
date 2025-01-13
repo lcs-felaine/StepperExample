fd68fb2013e1e7cd2fa201cefde70964441ddceb
Elaine Fang <efang@lcs.on.ca>
2025-01-13 08:49:20 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Fang Elaine on 2025/1/13.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
